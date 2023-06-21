076a780d9cfc2a6f168eaf985de5bf281bc965cc
UltraRossa <fa2um@yandex.ru>
2023-06-21 22:55:46 +0300

    refactoring genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -5,7 +5,6 @@ const makeObjectFromPath = (filepath) => JSON.parse(fs.readFileSync(filepath, { 
 const genDiff = (filepath1, filepath2) => {
   const obj1 = makeObjectFromPath(filepath1);
   const obj2 = makeObjectFromPath(filepath2);
-
   const unitedEntries = Object.entries({ ...obj1, ...obj2 }).sort(([key1], [key2]) => {
     if (key1 < key2) {
       return -1;
@@ -15,27 +14,22 @@ const genDiff = (filepath1, filepath2) => {
     }
     return 0;
   });
-
   const difference = unitedEntries.reduce((acc, [key, value]) => {
     if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
       const currentValue = `${acc}  - ${key}: ${value}\n`;
       return currentValue;
     }
-
     if (Object.hasOwn(obj2, key) && !Object.hasOwn(obj1, key)) {
       const currentValue = `${acc}  + ${key}: ${value}\n`;
       return currentValue;
     }
-
     if (obj1[key] === obj2[key]) {
       const currentValue = `${acc}    ${key}: ${value}\n`;
       return currentValue;
     }
-
     const currentValue = `${acc}  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}\n`;
     return currentValue;
   }, '');
-
   return `{\n${difference}}`;
 };
 
