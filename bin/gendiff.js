b62059453cc224dd3f48168ea1b9842b151dcbc0
UltraRossa <fa2um@yandex.ru>
2023-06-21 22:35:43 +0300

    update genDiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/genDiff.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -8,6 +9,7 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse();
