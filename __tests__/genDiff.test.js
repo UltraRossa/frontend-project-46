17cff341762a381d007714dcd5dcccf6343b686b
UltraRossa <fa2um@yandex.ru>
2023-06-24 23:33:13 +0300

    update tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,23 +1,17 @@
 import genDiff from '../src/index.js';
 import { readFile, getFixturePath } from '../src/utils.js';
 
-test('differ plain JSON files', () => {
+test('differ plain files', () => {
+  const expected = String(readFile(getFixturePath('expectedFile.txt')));
   const testPath1 = getFixturePath('testFile1.JSON');
   const testPath2 = getFixturePath('testFile2.JSON');
-  const expected = String(readFile(getFixturePath('expectedFile.txt')));
   expect(genDiff(testPath1, testPath2)).toEqual(expected);
-});
 
-test('differ plain YAML files with .yaml extension', () => {
-  const testPath1 = getFixturePath('testFile1.yaml');
-  const testPath2 = getFixturePath('testFile2.yaml');
-  const expected = String(readFile(getFixturePath('expectedFile.txt')));
-  expect(genDiff(testPath1, testPath2)).toEqual(expected);
-});
+  const testPath3 = getFixturePath('testFile1.yaml');
+  const testPath4 = getFixturePath('testFile2.yaml');
+  expect(genDiff(testPath3, testPath4)).toEqual(expected);
 
-test('differ plain YAML files with .yml extension', () => {
-  const testPath1 = getFixturePath('testFile1.yml');
-  const testPath2 = getFixturePath('testFile2.yml');
-  const expected = String(readFile(getFixturePath('expectedFile.txt')));
-  expect(genDiff(testPath1, testPath2)).toEqual(expected);
+  const testPath5 = getFixturePath('testFile1.yml');
+  const testPath6 = getFixturePath('testFile2.yml');
+  expect(genDiff(testPath5, testPath6)).toEqual(expected);
 });
