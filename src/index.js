6138cf5d9aaed6ed9fce7de4437bde0ce9df3f12
UltraRossa <fa2um@yandex.ru>
2023-07-03 23:35:21 +0300

    add stylish.js formatter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import parse from './parsers.js';
 import makeNewTree from './makeNewTree.js';
 import stylish from './stylish.js';
 
-const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
+const genDiff = (filepath1, filepath2) => {
   const fullPath1 = normalizePath(filepath1);
   const fullPath2 = normalizePath(filepath2);
   const parsedObject1 = parse(fullPath1);
@@ -11,7 +11,5 @@ const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   const diff = makeNewTree(parsedObject1, parsedObject2);
   return stylish(diff);
 };
-
-// genDiff('../__fixtures__/testFile1.JSON', '../__fixtures__/testFile2.JSON');
 
 export default genDiff;
