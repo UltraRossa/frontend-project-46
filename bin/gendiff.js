02ff7f0f699b77f6dc8c051327f60f687aee16a7
UltraRossa <fa2um@yandex.ru>
2023-07-05 02:35:22 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
