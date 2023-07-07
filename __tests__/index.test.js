6bf68f878d9b23afade6b4021248c84e0a21811d
UltraRossa <fa2um@yandex.ru>
2023-07-07 10:58:27 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,17 @@
 import genDiff from '../index.js';
 import { readFile, getFixturePath } from '../src/utils.js';
 
-let testPath1;
-let testPath2;
-let testPath3;
-let testPath4;
-let testPath5;
-let testPath6;
-
-beforeAll(() => {
-  testPath1 = getFixturePath('testFile1.json');
-  testPath2 = getFixturePath('testFile2.json');
-  testPath3 = getFixturePath('testFile1.yaml');
-  testPath4 = getFixturePath('testFile2.yaml');
-  testPath5 = getFixturePath('testFile1.yml');
-  testPath6 = getFixturePath('testFile2.yml');
-});
-
 test('gendiff with different formatters', () => {
+  const testPath1 = getFixturePath('testFile1.json');
+  const testPath2 = getFixturePath('testFile2.json');
+  const testPath3 = getFixturePath('testFile1.yaml');
+  const testPath4 = getFixturePath('testFile2.yaml');
+  const testPath5 = getFixturePath('testFile1.yml');
+  const testPath6 = getFixturePath('testFile2.yml');
   const expectedStylish = String(readFile(getFixturePath('expectedStylish.txt')));
   const expectedPlain = String(readFile(getFixturePath('expectedPlain.txt')));
   const expectedJson = String(readFile(getFixturePath('expectedJson.txt')));
+
   expect(genDiff(testPath3, testPath6)).toEqual(expectedStylish);
   expect(genDiff(testPath5, testPath4, 'stylish')).toEqual(expectedStylish);
   expect(genDiff(testPath3, testPath2, 'plain')).toEqual(expectedPlain);
