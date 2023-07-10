c95309740e6deabdbbeaec7bd27f3aff45164592
UltraRossa <fa2um@yandex.ru>
2023-07-10 14:25:53 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-import { getFullPath } from './src/utils.js';
-import format from './src/formatters/index.js';
-import parse from './src/parse.js';
-import makeDiffTree from './src/makeDiffTree.js';
-
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const fullPath1 = getFullPath(filepath1);
-  const fullPath2 = getFullPath(filepath2);
-  const parsedObject1 = parse(fullPath1);
-  const parsedObject2 = parse(fullPath2);
-  const diff = makeDiffTree(parsedObject1, parsedObject2);
-  const formattedDiff = format(formatName, diff);
-  return formattedDiff;
-};
+import genDiff from './src/index.js';
 
 export default genDiff;
