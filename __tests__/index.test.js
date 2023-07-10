1f76667af4172f5c631ed36fd91776d0e642154f
UltraRossa <fa2um@yandex.ru>
2023-07-10 14:25:02 +0300

    upadte __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,27 @@
-import genDiff from '../index.js';
-import { readFile, getFixturePath } from '../src/utils.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+import { readFile } from '../src/parse.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('gendiff with different formatters', () => {
-  const testPath1 = getFixturePath('testFile1.json');
-  const testPath2 = getFixturePath('testFile2.json');
-  const testPath3 = getFixturePath('testFile1.yaml');
-  const testPath4 = getFixturePath('testFile2.yaml');
-  const testPath5 = getFixturePath('testFile1.yml');
-  const testPath6 = getFixturePath('testFile2.yml');
+  const testPathJson1 = getFixturePath('testFile1.json');
+  const testPathJson2 = getFixturePath('testFile2.json');
+  const testPathYaml1 = getFixturePath('testFile1.yaml');
+  const testPathYaml2 = getFixturePath('testFile2.yaml');
+  const testPathYml1 = getFixturePath('testFile1.yml');
+  const testPathYml2 = getFixturePath('testFile2.yml');
   const expectedStylish = String(readFile(getFixturePath('expectedStylish.txt')));
   const expectedPlain = String(readFile(getFixturePath('expectedPlain.txt')));
   const expectedJson = String(readFile(getFixturePath('expectedJson.txt')));
 
-  expect(genDiff(testPath3, testPath6)).toEqual(expectedStylish);
-  expect(genDiff(testPath5, testPath4, 'stylish')).toEqual(expectedStylish);
-  expect(genDiff(testPath3, testPath2, 'plain')).toEqual(expectedPlain);
-  expect(genDiff(testPath1, testPath6, 'plain')).toEqual(expectedPlain);
-  expect(genDiff(testPath5, testPath4, 'json')).toEqual(expectedJson);
+  expect(genDiff(testPathYaml1, testPathJson2)).toEqual(expectedStylish);
+  expect(genDiff(testPathYml1, testPathYaml2, 'stylish')).toEqual(expectedStylish);
+  expect(genDiff(testPathYaml1, testPathJson2, 'plain')).toEqual(expectedPlain);
+  expect(genDiff(testPathJson1, testPathYml2, 'plain')).toEqual(expectedPlain);
+  expect(genDiff(testPathYml1, testPathYaml2, 'json')).toEqual(expectedJson);
 });
