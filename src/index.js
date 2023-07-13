b95284638885ceb26fcb8c0698b6f95edf0fd0d8
UltraRossa <fa2um@yandex.ru>
2023-07-13 14:54:15 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
+import fs from 'fs';
 import path from 'path';
 import format from './formatters/index.js';
-import parse from './parse.js';
+import parse from './parsers.js';
 import makeDiffTree from './makeDiffTree.js';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getExtension = (filepath) => path.extname(filepath);
 
+export const readFile = (filepath) => fs.readFileSync(filepath, { encoding: 'utf8' });
+
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const getObjectFromFile1 = parse(getExtension(filepath1));
-  const getObjectFromFile2 = parse(getExtension(filepath2));
-  const fullPath1 = getFullPath(filepath1);
-  const fullPath2 = getFullPath(filepath2);
-  const object1 = getObjectFromFile1(fullPath1);
-  const object2 = getObjectFromFile2(fullPath2);
-  const diff = makeDiffTree(object1, object2);
+  const parsedObject1 = parse(readFile(getFullPath(filepath1)), getExtension(filepath1));
+  const parsedObject2 = parse(readFile(getFullPath(filepath2)), getExtension(filepath2));
+  const diff = makeDiffTree(parsedObject1, parsedObject2);
   const formattedDiff = format(formatName, diff);
   return formattedDiff;
 };
